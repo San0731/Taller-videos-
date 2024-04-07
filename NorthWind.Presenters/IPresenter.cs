41021382018e93e3ac856f34e455d835a5520a40
San0731 <128946767+San0731@users.noreply.github.com>
2024-04-07 13:05:07 -0500

    Commit 16 CreateOrederPresenter

---
+++ NorthWind.Presenters/IPresenter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using NorthWind.UseCases.Common.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Presenters
+{
+    public interface IPresenter<REesponseType, FormatType>: IOutputPort<ResponseType>
+    {
+        public FormatType Content { get; }
+    }
+}
